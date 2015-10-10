54823835cb5460d057521c4be6154a687f048278
Jonathan Bosmans <jonathan.bosmans@be.ibm.com>
2015-10-10 19:16:42 +0200

    Almost ready

--- spec/fsops-spec.js
+++ spec/fsops-spec.js
@@ -3,17 +3,8 @@ var path = require("path");
 var fs = require("fs");
 var filesDir = path.resolve(path.dirname(__filename) , "files");
 var listDir = path.resolve(filesDir, "listDir");
-/*
- copy:copyUpdated,
- deleteRecursively:deleteRecursively,
- mkdirsSync:mkdirsSync,
- listDirChildrenFullPathsRecursively:listDirChildrenFullPathsRecursively,
- listDirChildrenFullPathsRecursivelyFull:listDirChildrenFullPathsRecursivelyFull,
- ensureParentDirExists:ensureParentDirExists
- */
 
 describe("fsops", function(){
-
     it("can list recursively", function(){
         var children = fsops.listDirChildrenFullPathsRecursivelyFull(listDir);
         expect(children.length).toBe(3);
@@ -41,13 +32,12 @@ describe("fsops", function(){
         expect(fs.existsSync(newPath)).toBe(false);
         fsops.mkdirsSync(newPath);
         expect(fs.statSync(newPath).isDirectory()).toBe(true);
-        fs.rmdirSync(filesDir + path.sep + "a" + path.sep + "b" + path.sep + "c");
-        fs.rmdirSync(filesDir + path.sep + "a" + path.sep + "b" );
-        fs.rmdirSync(filesDir + path.sep + "a");
+        fsops.deleteRecursively(filesDir + path.sep + "a");
     });
     it("can copy recursively", function(){
         var copyDirPath = path.resolve(filesDir, "listDirCopy");
-        fsops.copyUpdated(listDir, copyDirPath);
+        var changeCount = fsops.copyUpdated(listDir, copyDirPath);
+        expect(changeCount).toBe(4);
         expect(fs.statSync(copyDirPath).isDirectory()).toBe(true);
 
         var file1Path = path.resolve(copyDirPath, "file1.txt");
@@ -55,10 +45,7 @@ describe("fsops", function(){
         var nestedDirPath = path.resolve(copyDirPath, "nested");
         var file2Path = path.resolve(nestedDirPath, "file2.txt");
         expect(fs.readFileSync(file2Path).toString()).toBe('file2');
-        fs.unlinkSync(file2Path);
-
-        fs.rmdirSync(nestedDirPath);
-        fs.rmdirSync(copyDirPath);
+        fsops.deleteRecursively(copyDirPath);
     });
     it("can delete recursively", function(){
         var copyDirPath = path.resolve(filesDir, "listDirCopy");
@@ -72,13 +59,16 @@ describe("fsops", function(){
         var applyToDir = [filesDir, "applyToDir"].join(path.sep);
         var changedDirPath = [filesDir, "changedDir"].join(path.sep);
         fsops.copyUpdated(listDir, applyToDir);
-        expect(fsops.showChangesToApply(listDir, applyToDir).changeCount).toBe(0);
+        var changed = fsops.showChangesToReapply(listDir, applyToDir);
+        fsops.logDiffSummary(changed);
+        expect(changed.changeCount).toBe(0);
         fsops.copyUpdated(listDir, changedDirPath);
         fs.writeFileSync(path.resolve(changedDirPath, "file3.txt"), "file3");
         fs.writeFileSync([changedDirPath, "nested", "file2.txt"].join(path.sep), "file2mod");
         fs.unlinkSync(path.resolve(changedDirPath, "file1.txt"));
-        var o = fsops.showChangesToApply(changedDirPath, applyToDir);
-        console.log("changes to apply: ", o);
+        var o = fsops.showChangesToReapply(changedDirPath, applyToDir);
+        console.log("changes to apply: ");
+        fsops.logDiffSummary(o);
         expect(o.changeCount >0).toBe(true);
         expect(o.deleted.length).toBe(1);
         expect(o.added.length).toBe(1);
@@ -113,7 +103,6 @@ describe("fsops", function(){
         var changedDirPath = [filesDir, "changedDir"].join(path.sep);
         fsops.copyUpdated(listDir, changedDirPath);
         expect(fsops.equalContent(listDir, changedDirPath)).toBe(true);
-
         var file3Path = path.resolve(changedDirPath, "file3.txt");
         fs.writeFileSync(file3Path, "file3");
         expect(fsops.equalContent(listDir, changedDirPath)).toBe(false);
@@ -129,66 +118,21 @@ describe("fsops", function(){
         expect(fsops.equalContent(listDir, changedDirPath)).toBe(false);
         fsops.deleteRecursively(changedDirPath);
     });
-    xit("can do a compare of theme sources :)", function(){
-        //var source = '/home/spectre/toMergeOct8/source';
-        //var target = '/home/spectre/Projects/IBM/DSV/angularTheme';
-
-        //var source = '/tmp/newMerge/theme/angularTheme-static/src/main/webapp/themes/angularTheme';
-        //var target = '/home/spectre/Projects/IBM/DSV/angularTheme/angularTheme-static/src/main/webapp/themes/angularTheme';
-
-
-        var o = fsops.showChangesToApplyFlat(source, target, ["*-xmlaccess*.xml", "*.iml", "*/.idea",".idea",".git", ".project", ".settings", "*/dav/fs-type1"]);
-        //delete o.unchanged;
+    it("can do a compare of theme sources :)", function(){
+        pending("currently depends on dev machine");
+        var source = '/home/spectre/Projects/IBM/DSV/angularTheme/angularTheme-static/src/main/webapp/themes/angularTheme';
+        var target = '/mnt/gentoo64/home/spectre/Projects/DSV/angularTheme/angularTheme-static/src/main/webapp/themes/angularTheme';
+        var o = fsops.showChangesToReapply(source, target, ["*-xmlaccess*.xml", "*.iml", "*/.idea",".idea",".git", ".project", ".settings", "*/dav/fs-type1"]);
         console.log("WOULD MERGE: ");
-        logChanged(o);
-
-        //o.statusChanged.forEach(function(s){
-        //    var diff = "diff " + source + path.sep + s.path.relativePath + " " + target + path.sep + s.path.relativePath;
-        //
-        //        console.log(spaceOut(s.reason, 15) + s.path.relativePath + (s.reason === 'updated' ? (" :: " + diff) : "")) ;
-        //
-        //
-        //});
-        //console.log('Changing ' + o.statusChanged.length + " files/dirs");
-        //if(false)
-        //    fsops.reapplyChangesToDir(source, target, ["*-xmlaccess*.xml", "*.iml", ".idea",".git", ".project", ".settings"]);
+        fsops.logDiffSummary(o)
 
     });
-    function logChanged(output){
-        output.statusChanged.forEach(function(s){
-            var diff = "diff " + output.changedDir + path.sep + s.path.relativePath + " " + output.applyToDir + path.sep + s.path.relativePath;
-            console.log(spaceOut(s.reason, 15) + s.path.relativePath + (s.reason === 'updated' ? (" :: " + diff) : "")) ;
-        });
-        console.log('Found ' + output.statusChanged.length + " changes to files/dirs");
-    }
 
-    function spaceOut(str, length){
-        var s= str;
-        while(s.length < length){
-            s+= ' ';
-        }
-        return s;
-    }
-
-    fit("can properly ignore non readable files, dirs etc", function(){
+    it("can properly ignore non readable files, dirs etc", function(){
+        pending("currently depends on dev machine");
         var source = '/home/spectre/Projects/IBM/DSV/angularTheme';
         var target = '/mnt/gentoo64/home/spectre/Projects/DSV/angularTheme';
-
-        var o = fsops.showChangesToApply(source, target, [".git", ".idea"]);
+        var o = fsops.showChangesToReapply(source, target, [".git", ".idea"]);
         fsops.logDiffSummary(o);
-        console.log(o.unreadablePaths.map(function(c){
-            return "UNREADABLE "+c.path;
-        }))
-        //var summary = fsops.createSummaryOfChanges(o);
-        //summary.forEach(function(sp){
-        //    console.log(sp.status + (sp.path.dir ? "d " : "f ") + sp.path.relativePath);
-        //});
-        //logChanged(o);
-        //console.log("DELETED TOP LEVEL DIRS = ", o.deletedTopLevel.map(function(c){
-        //    return (c.dir ? "d " : "f ") + c.path;
-        //}));
-        //console.log("all deleted = " + o.deleted.length  + "  vs  top level = " + o.deletedTopLevel.length);
     })
-
-
 });
